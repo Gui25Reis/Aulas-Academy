14c989e378a2d6275bc1bdf64fa50a857481b78d
Gui Reis <gui.sreis25@gmail.com>
2022-05-31 21:09:24 -0300

    Aula Swift Lint

---
+++ Aulas/Swift Lint/SwiftLint-Aula/SwiftLint-Aula/Controller/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SwiftLint-Aula
+//
+//  Created by Gui Reis on 29/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // swiftlint:disable fatal_error
+    let teste1 = 7
+    // swiftlint:enable fatal_error
+    
+    
+    // swiftlint:disable [fatal_error, space_error]
+    let teste2 = 7
+    // swiftlint:enable [fatal_error, space_error]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
