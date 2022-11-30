0ee2dd6950e979f0b50143fb878e5c778d793585
Gui Reis <gui.sreis25@gmail.com>
2022-11-30 04:27:00 -0300

    Subindo aula watchOS

---
+++ Aulas/WatchOS/Aula-WatchOS Watch App/Aula_WatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aula_WatchOSApp.swift
+//  Aula-WatchOS Watch App
+//
+//  Created by Gui Reis on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Aula_WatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
